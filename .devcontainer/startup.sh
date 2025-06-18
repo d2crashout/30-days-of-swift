56cadac591a5a9af06af10285e83a2b5f0a09a8f
Abhimanyu <methebestestofall@gmail.com>
2025-06-18 19:37:38 +0000

    finally fixed swift install config

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -4,4 +4,8 @@ tar zxf swiftly-$(uname -m).tar.gz && \
 ./swiftly init --quiet-shell-followup -y && \
 . "${SWIFTLY_HOME_DIR:-$HOME/.local/share/swiftly}/env.sh" && \
 hash -r
+. "$HOME/.local/share/swiftly/env.sh"
 
+# swift dependencies
+sudo apt-get update
+sudo apt-get -y install libpython3-dev libxml2-dev libz3-dev pkg-config python3-lldb-13
