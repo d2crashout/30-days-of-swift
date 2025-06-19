f2e157009916a77abe282860cfedf9b7c53772af
Abhimanyu <methebestestofall@gmail.com>
2025-06-19 12:04:57 +0000

    did part of day 2! will add more to day 2 today! it will be string interpolation next btw

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -11,3 +11,10 @@ sudo apt-get update
 sudo apt-get -y install libpython3-dev libxml2-dev libz3-dev pkg-config python3-lldb-13
 
 echo "Run: . "$HOME/.local/share/swiftly/env.sh""
+
+export SWIFTLY_HOME_DIR="/home/vscode/.local/share/swiftly"
+export SWIFTLY_BIN_DIR="/home/vscode/.local/share/swiftly/bin"
+export SWIFTLY_TOOLCHAINS_DIR="/home/vscode/.local/share/swiftly/toolchains"
+if [[ ":$PATH:" != *":$SWIFTLY_BIN_DIR:"* ]]; then
+    export PATH="$SWIFTLY_BIN_DIR:$PATH"
+fi
---
+++ day 2/main.swift
@@ -0,0 +1,19 @@
+// learned about variables and how to assign types
+var year: Int = 2025 
+var day: Int = 1
+var hour: Double
+var month: String
+hour = 9
+month = "June"
+// learned about assignment operators
+day += 1
+print(day) // should be two
+var apples = 100
+apples -= 10
+print(apples) // should equal 90
+apples *= 1
+print(apples) // should equal 90
+apples /= 9 
+print(apples) // should be 10
+apples %= 2
+print(apples) // should be 0
